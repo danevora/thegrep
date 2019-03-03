0a5643be09f9315d1ea6f89d1258131138a7fdab
Daniel <daniel.evora764@gmail.com>
2019-03-03 20:14:52 +0000

    adding names in cargo and main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 /**
  * thegrep - Tar Heel egrep
  *
- * Author(s): 
- * ONYEN(s): 
+ * Author(s): Daniel Evora, Peter Morrow 
+ * ONYEN(s): devora, peterjm
  *
  * UNC Honor Pledge: I pledge I have received no unauthorized aid
  * on this assignment. I further pledge not to distribute my solution
