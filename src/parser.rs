4a5e6b055a9fb452638e19a7f2080b8dfab6fcbb
Daniel <daniel.evora764@gmail.com>
2019-03-07 16:26:30 +0000

    comments in parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -39,6 +39,7 @@ impl<'tokens> Parser<'tokens> {
             tokens: tokenizer.peekable(),
         };
         let mut parse = parser.reg_expr();
+        //checking to make sure the parser accounted for all tokens in input
         let next = parser.take_next_token();
         match next {
             Ok(token) => Err(format!("Expected end of input, found {:?}", token)),
@@ -49,8 +50,13 @@ impl<'tokens> Parser<'tokens> {
 
 //Add Tests here
 
+//helper functions for implementing thegrep grammar
 impl<'tokens> Parser<'tokens> {
 
+    //reg_expr calls for a catenation and then checks to see if a union bar is present. If there
+    //is, reg_expr is called again and a alternation expression is created with the original expr
+    //and the one that was just retrieved with ast_two. If there is no union bar, then just the
+    //original expr is returned
     fn reg_expr(&mut self) -> Result<AST, String> {
         let ast = self.cat()?;
         if let Some(token) = self.tokens.peek(){
@@ -68,6 +74,10 @@ impl<'tokens> Parser<'tokens> {
         
     }
 
+    //cat calls clo and keeps working along the grammar, the result of calling clo is stored in
+    //closure. Next, we check to see if there is anything we need to catenate this value with. If
+    //there is any token that signifies a new atom, we call clo again. Otherwise, we simply return
+    //the "first" closure
     fn cat(&mut self) -> Result<AST, String> {
         let closure = self.clo()?;
         if let Some(token) = self.tokens.peek() {
@@ -84,7 +94,10 @@ impl<'tokens> Parser<'tokens> {
         }
         
     }
-
+    
+    //clo calls atom and stores this in a variable. Next, we check to see if this atom has a kleene
+    //star after it. If so, we return a closure that enveloped this atom, otherwise, it simply
+    //returns the atom
     fn clo(&mut self) -> Result<AST, String> {
         let atom = self.atom()?;
         if let Some(kleene) = self.tokens.peek() {
@@ -100,7 +113,9 @@ impl<'tokens> Parser<'tokens> {
         }
     }
 
-
+    //atom deals with the most basic building blocks of the grammar. If there is a Lparen, we look
+    //for the reg_expr inside of it, if there is AnyChar, we return AnyChar, and if there is a char
+    //we just return an AST char object enveloping the character. 
     fn atom(&mut self) -> Result<AST, String> {
         let next = self.take_next_token();
         match next {
@@ -118,8 +133,11 @@ impl<'tokens> Parser<'tokens> {
     }
 }
 
+//helper functions for parsing
 impl<'tokens> Parser<'tokens> {
-
+    
+    //this functions moves the iterator over the tokens forward and returns the token that was
+    //next, or returns an error if this method was called and there were no more tokens
     fn take_next_token(&mut self) -> Result<Token, String> {
         if let Some(token) = self.tokens.next() {
             Ok(token)
@@ -128,6 +146,9 @@ impl<'tokens> Parser<'tokens> {
         }
     }
 
+    //this function does the same thing as take_next_token except it allows us to pass in an
+    //expected value of the next token and if they don't match, returns an error, otherwise it
+    //returns the expected token (given it exists)
     fn consume_token(&mut self, expected: Token) -> Result<Token, String> {
         if let Some(next) = self.tokens.next() {
             if next != expected {
